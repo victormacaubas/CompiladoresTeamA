3e28e46f2da270bdbeaaa2578ecf3c09cfdd167b
Victor Macaubas <victor@macaubas.com>
2022-09-27 09:00:55 -0300

    estado 0

--- compilador-main/CompiladorL3/src/compiladorl3/Lexico.java
+++ compilador-main/CompiladorL3/src/compiladorl3/Lexico.java
@@ -21,127 +21,145 @@ import java.util.logging.Logger;
 public class Lexico {
     private char[] conteudo;
     private int indiceConteudo;
-    
-    public Lexico(String caminhoCodigoFonte){
+
+    public Lexico(String caminhoCodigoFonte) {
         try {
             String conteudoStr;
             conteudoStr = new String(Files.readAllBytes(Paths.get(caminhoCodigoFonte)));
             this.conteudo = conteudoStr.toCharArray();
-            this.indiceConteudo = 0;                        
+            this.indiceConteudo = 0;
         } catch (IOException ex) {
             ex.printStackTrace();
-        }        
+        }
     }
-    
-    //Retorna próximo char
-    private char nextChar(){
+
+    // Retorna próximo char
+    private char nextChar() {
         return this.conteudo[this.indiceConteudo++];
     }
-    
-    //Verifica existe próximo char ou chegou ao final do código fonte
-    private boolean hasNextChar(){
+
+    // Verifica existe próximo char ou chegou ao final do código fonte
+    private boolean hasNextChar() {
         return indiceConteudo < this.conteudo.length;
     }
-    
-    //Retrocede o índice que aponta para o "char da vez" em uma unidade
-    private void back(){
+
+    // Retrocede o índice que aponta para o "char da vez" em uma unidade
+    private void back() {
         this.indiceConteudo--;
     }
-    
-    //Identificar se char é letra minúscula    
-    private boolean isLetra(char c){
+
+    // Identificar se char é letra minúscula
+    private boolean isLetra(char c) {
         return (c >= 'a') && (c <= 'z');
     }
-    
-    //Identificar se char é dígito
-    private boolean isDigito(char c){
+
+    // Identificar se char é dígito
+    private boolean isDigito(char c) {
         return (c >= '0') && (c <= '9');
     }
-    
-    //Método retorna próximo token válido ou retorna mensagem de erro.
-    public Token nextToken(){
+
+    // Método retorna próximo token válido ou retorna mensagem de erro.
+    public Token nextToken() {
         Token token = null;
         char c;
         int estado = 0;
-        
+
         StringBuffer lexema = new StringBuffer();
-        while(this.hasNextChar()){
-            c = this.nextChar();            
-            switch(estado){
+        while (this.hasNextChar()) {
+            c = this.nextChar();
+            switch (estado) {
                 case 0:
-                    if(c == ' ' || c == '\t' || c == '\n' || c == '\r' ){ //caracteres de espaço em branco ASCII tradicionais 
+                    if (c == ' ' || c == '\t' || c == '\n' || c == '\r') { // caracteres de espaço em branco ASCII
+                                                                           // tradicionais
                         estado = 0;
-                    }
-                    else if(this.isLetra(c) || c == '_'){
+                    } else if (this.isLetra(c) || c == '_') {
+                        lexema.append(c);
+                        estado = 7;
+                    } else if (this.isDigito(c)) {
                         lexema.append(c);
                         estado = 1;
-                    }
-                    else if(this.isDigito(c)){
-                        lexema.append(c);
-                        estado = 2;
-                    }
-                    else if(c == ')' || 
+                    } else if (c == ')' ||
                             c == '(' ||
                             c == '{' ||
                             c == '}' ||
                             c == ',' ||
-                            c == ';'){
+                            c == ';') {
                         lexema.append(c);
-                        estado = 5;
-                    }else if(c == '$'){
+                        estado = 15;
+                    } else if (c == '$') {
                         lexema.append(c);
-                        estado = 99;
+                        estado = 18;
                         this.back();
-                    }else{
+                    } else if (c == '<') {
+                        lexema.append(c);
+                        estado = 8;
+                    } else if (c == '>') {
+                        lexema.append(c);
+                        estado = 11;
+                    } else if (c == '=') {
+                        lexema.append(c);
+                        estado = 13;
+                    } else if (c == '+' ||
+                            c == '-' ||
+                            c == '*' ||
+                            c == '/' ||
+                            c == '%') {
+                        lexema.append(c);
+                        estado = 16;
+                    }else if (c == ' ') {
+                        lexema.append(c);
+                        estado = 13;
+                    }
+                     else {
                         lexema.append(c);
                         throw new RuntimeException("Erro: token inválido \"" + lexema.toString() + "\"");
                     }
                     break;
                 case 1:
-                    if(this.isLetra(c) || this.isDigito(c) || c == '_'){
+                    if (this.isLetra(c) || this.isDigito(c) || c == '.') {
                         lexema.append(c);
-                        estado = 1;                        
-                    }else{
+                        estado = 1;
+                    } else {
                         this.back();
-                        return new Token(lexema.toString(), Token.TIPO_IDENTIFICADOR);                        
+                        return new Token(lexema.toString(), Token.TIPO_IDENTIFICADOR);
                     }
                     break;
                 case 2:
-                    if(this.isDigito(c)){
+                    if (this.isDigito(c)) {
                         lexema.append(c);
                         estado = 2;
-                    }else if(c == '.'){
+                    } else if (c == '.') {
                         lexema.append(c);
                         estado = 3;
-                    }else{
+                    } else {
                         this.back();
                         return new Token(lexema.toString(), Token.TIPO_INTEIRO);
                     }
                     break;
                 case 3:
-                    if(this.isDigito(c)){
+                    if (this.isDigito(c)) {
                         lexema.append(c);
                         estado = 4;
-                    }else{
+                    } else {
                         throw new RuntimeException("Erro: número float inválido \"" + lexema.toString() + "\"");
                     }
                     break;
                 case 4:
-                    if(this.isDigito(c)){
+                    if (this.isDigito(c)) {
                         lexema.append(c);
                         estado = 4;
-                    }else{
+                    } else {
                         this.back();
                         return new Token(lexema.toString(), Token.TIPO_REAL);
                     }
                     break;
                 case 5:
                     this.back();
-                    return new Token(lexema.toString(), Token.TIPO_CARACTER_ESPECIAL); 
+                    return new Token(lexema.toString(), Token.TIPO_CARACTER_ESPECIAL);
                 case 99:
-                    return new Token(lexema.toString(), Token.TIPO_FIM_CODIGO); 
+                    return new Token(lexema.toString(), Token.TIPO_FIM_CODIGO);
             }
-        }                
+        }
         return token;
-    }   
+    }
 }
