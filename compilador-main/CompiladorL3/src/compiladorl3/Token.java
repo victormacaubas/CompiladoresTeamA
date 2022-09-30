904df6a4fa756ecfe5ec24c39cfd172249984c50
Victor Macaubas <victor@macaubas.com>
2022-09-30 08:56:49 -0300

    Final implementation

--- compilador-main/CompiladorL3/src/compiladorl3/Token.java
+++ compilador-main/CompiladorL3/src/compiladorl3/Token.java
@@ -15,34 +15,33 @@ public class Token {
     public static int TIPO_CHAR = 2;
     public static int TIPO_IDENTIFICADOR = 3;
     public static int TIPO_OPERADOR_RELACIONAL = 4;
-    public static int TIPO_OPERADOR_ATRIBUICAO = 13;
-    public static int TIPO_OPERADOR_ARITMETICO = 5;
-    public static int TIPO_CARACTER_ESPECIAL = 6;
-    public static int TIPO_PALAVRA_RESERVADA = 7;
-    public static int TIPO_FIM_CODIGO = 99;
-    
-    private int tipo; //tipo do token
-    private String lexema; //conteúdo do token
-    
-    public Token(String lexema, int tipo){
+    public static int TIPO_OPERADOR_ATRIBUICAO = 5;
+    public static int TIPO_OPERADOR_ARITMETICO = 6;
+    public static int TIPO_CARACTER_ESPECIAL = 7;
+    public static int TIPO_PALAVRA_RESERVADA = 8;
+    public static int TIPO_FIM_CODIGO = 9;
+
+    private int tipo; // tipo do token
+    private String lexema; // conteúdo do token
+
+    public Token(String lexema, int tipo) {
         this.lexema = lexema;
         this.tipo = tipo;
     }
-    
-    public String getLexema(){
+
+    public String getLexema() {
         return this.lexema;
     }
-    
-    public int getTipo(){
+
+    public int getTipo() {
         return this.tipo;
     }
-    
+
     @Override
-    public String toString()
-    {
-        switch(this.tipo){
+    public String toString() {
+        switch (this.tipo) {
             case 0:
-                return this.lexema + " - INTEIRO" ;
+                return this.lexema + " - INTEIRO";
             case 1:
                 return this.lexema + " - REAL";
             case 2:
@@ -52,16 +51,17 @@ public class Token {
             case 4:
                 return this.lexema + " - OPERADOR_RELACIONAL";
             case 5:
-                return this.lexema + " - OPERADOR_ARITMETICO";
+                return this.lexema + " - OPERADOR_ATRIBUIÇÃO";
             case 6:
-                return this.lexema + " - CARACTER_ESPECIAL";
+                return this.lexema + " - OPERADOR_ARITMETICO";
             case 7:
+                return this.lexema + " - CARACTER_ESPECIAL";
+            case 8:
                 return this.lexema + " - PALAVRA_RESERVADA";
-            case 99:
-                return this.lexema + " - FIM_CODIGO";    
+            case 9:
+                return this.lexema + " - FIM_CODIGO";
         }
         return "";
     }
-    
-    
+
 }
