b5a9d5163b20cc6aa0fb18caed7fed7ad521bded
Victor Macaubas <victor@macaubas.com>
2022-11-08 08:49:04 -0300

    Implementação inicial do sintático

--- compilador-main/CompiladorL3/src/compiladorl3/CompiladorL3.java
+++ compilador-main/CompiladorL3/src/compiladorl3/CompiladorL3.java
@@ -17,11 +17,15 @@ public class CompiladorL3 {
     public static void main(String[] args) {
         // TODO code application logic here
         Lexico lexico = new Lexico("src\\compiladorl3\\codigo.txt");
-        Token t = null;
+        //Token t = null;
+        /* 
         while((t = lexico.nextToken()) != null){
             System.out.println(t.toString());
         }
+        */
 
+        Sintatico1 sintatico = new Sintatico1(lexico);
+        sintatico.S();
 
     }
     
---
+++ compilador-main/CompiladorL3/src/compiladorl3/Sintatico1.java
@@ -0,0 +1,66 @@
+package compiladorl3;
+
+public class Sintatico1 {
+
+    private Lexico lexico;
+    private Token token;
+
+    public Sintatico1(Lexico lexico) {
+        this.lexico = lexico;
+    }
+
+    public void S() {
+        this.token = this.lexico.nextToken();
+        this.E();
+
+        if (this.token.getTipo() == Token.TIPO_FIM_CODIGO) {
+
+            System.out.println("O Código está massa! Arretado! Tu botou pra torar");
+        }
+    }
+
+    private void E() {
+        this.T();
+        this.El();
+
+    }
+
+    private void El() {
+
+        if (this.token.getTipo() == Token.TIPO_OPERADOR_ARITMETICO) {
+
+            this.OP();
+            this.T();
+            this.El();
+
+        } else {
+
+        }
+
+    }
+
+    private void T() {
+
+        if (this.token.getTipo() == Token.TIPO_IDENTIFICADOR || this.token.getTipo() == Token.TIPO_INTEIRO
+                || this.token.getTipo() == Token.TIPO_REAL) {
+
+            this.token = this.lexico.nextToken();
+
+        } else {
+            throw new RuntimeException(
+                    "Oxe, era para ser um identificador " + "ou número pertinho de " + this.token.getLexema());
+        }
+    }
+
+    private void OP() {
+
+        if (this.token.getTipo() == Token.TIPO_OPERADOR_ARITMETICO) {
+            this.token = this.lexico.nextToken();
+
+        } else {
+            throw new RuntimeException(
+                    "Oxe, era para ser um operador aritmético (+, -, /, *) pertinho de " + this.token.getLexema());
+        }
+    }
+
+}
