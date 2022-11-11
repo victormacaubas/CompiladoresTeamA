e223d72126fcc2396fa61293a39620b293ab36bf
Victor Macaubas <victor@macaubas.com>
2022-11-11 08:19:13 -0300

    edit in main

--- compilador-main/CompiladorL3/src/compiladorl3/CompiladorL3.java
+++ compilador-main/CompiladorL3/src/compiladorl3/CompiladorL3.java
@@ -3,7 +3,7 @@ package compiladorl3;
 public class CompiladorL3 {
 
     public static void main(String[] args) {
-        Lexico lexico = new Lexico("/workspace/CompiladoresTeamA/compilador-main/CompiladorL3/src/compiladorl3/codigo.txt");
+        Lexico lexico = new Lexico("src\\compiladorl3\\codigo.txt");
 
         Sintatico1 sintatico = new Sintatico1(lexico);
         sintatico.S();
