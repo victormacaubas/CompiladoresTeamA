51a853a47465f176661b057e90164c8a10749f91
Victor Macaubas <victor@macaubas.com>
2022-11-29 15:45:31 -0300

    Desenvolvimento de métodos do sintatico

--- compilador-main/CompiladorL3/src/compiladorl3/Sintatico1.java
+++ compilador-main/CompiladorL3/src/compiladorl3/Sintatico1.java
@@ -1,5 +1,7 @@
 package compiladorl3;
 
+import compiladorl3.Exepctions.SintaticException;
+
 public class Sintatico1 {
 
     private Lexico lexico;
@@ -11,48 +13,213 @@ public class Sintatico1 {
 
     public void S() {
         this.token = this.lexico.nextToken();
-        this.E();
-        
+
+        if (!this.token.getLexema().equals("int")) {
+            throw new SintaticException("O int deve ser no inicio");
+        }
+
+        this.token = this.lexico.nextToken();
+
+        if (!this.token.getLexema().equals("main")) {
+            throw new SintaticException("Colocoque a main");
+        }
+
+        this.token = this.lexico.nextToken();
+
+        if (!this.token.getLexema().equals("(")) {
+            throw new SintaticException("Abrir o parenteses do main");
+        }
+
+        this.token = this.lexico.nextToken();
+
+        if (!this.token.getLexema().equals(")")) {
+            throw new SintaticException("Fechar o parenteses do main");
+        }
+
+        this.token = this.lexico.nextToken();
+
+        this.Bloco();
+
         if (this.token.getTipo() == Token.TIPO_FIM_CODIGO) {
             System.out.println("O Código está massa! Arretado! Tu botou pra torar");
         }
     }
 
-    private void E() {
-        this.T();
-        this.El();
+    private void Bloco() {
+
+        if (!this.token.getLexema().equals("{")) {
+            throw new SintaticException("É necessesario abrir chaves => { Encontramos => " + this.token.getLexema().toString());
+        }
+
+        this.token = this.lexico.nextToken();
+
+        DecVariable();
+
+        while (this.token.getTipo() == Token.TIPO_IDENTIFICADOR || this.token.getLexema().equals("{") || this.token.getLexema().equals("while") || this.token.getLexema()equals("if")) {
+            this.Comando();
+            
+        }
+
+        if (!token.getLexema().equals("}")) {
+            throw new SintaticException("É necessesario fechar as chaves => { Encontramos => " + this.token.getLexema().toString());
+        }
+
+        this.token = this.lexico.nextToken();
+
+
     }
 
-    private void El() {
-        if (this.token.getTipo() == Token.TIPO_OPERADOR_ARITMETICO) {
-            this.OP();
-            this.T();
-            this.El();
+    private void Comando() {
+        if (this.token.getTipo() == Token.TIPO_IDENTIFICADOR || this.token.getLexema().equals("{")) {
+            this.ComandoBasico();
 
-        } else {
+        } else if (this.token.getLexema().equals("while")) {
+            this.Iterations();
+
+        } else if (this.token.getLexema().equals("if")) {
+            this.token = this.lexico.nextToken();
+
+            if (!this.token.getLexema().equals("(")) {
+                throw new SintaticException(
+                        "( esperado para finalizar a declaração, ao lado de =>  " + this.token.getLexema().toString());
+            }
+
+            this.token = this.lexico.nextToken();
+            this.relationalExp();
+
+            if (!this.token.getLexema().equals(")")) {
+                throw new SintaticException(
+                        ") esperado para finalizar a declaração, ao lado de =>  " + this.token.getLexema().toString());
+            }
+
+            this.token = this.lexico.nextToken();
+            this.Comando();
+
+            if (token.getLexema().equals("else")) {
+                this.token = this.lexico.nextToken();
+                this.Comando();
+            }
+        }
+
+    }
+
+    private void ComandoBasico() {
+
+        if (this.token.getTipo() == Token.TIPO_IDENTIFICADOR) {
+            this.Attr();
+        }
+
+        if (this.token.getLexema().equals("{")) {
+            this.Bloco();
+        }
+
+    }
+
+    private void Attr() {
+
+        if (this.token.getTipo() != Token.TIPO_IDENTIFICADOR) {
+            throw new SintaticException(
+                    "é necessário declarar uma variável, foi encontrado => " + this.token.getLexema().toString());
 
         }
+
+        this.token = this.lexico.nextToken();
+
+        if (!this.token.getLexema().equals("=")) {
+            throw new SintaticException(
+                    "= esperado para atribuir um valor, foi encontrado => " + this.token.getLexema().toString());
+
+        }
+
+        this.token = this.lexico.nextToken();
+
+        this.aritmeticExp();
+
+        if (!this.token.getLexema().equals(";")) {
+            throw new SintaticException(
+                    "; esperado para finalizar a declaração, ao lado de => " + this.token.getLexema().toString());
+        }
+
+        this.token = this.lexico.nextToken();
+
     }
 
-    private void T() {
-        if (this.token.getTipo() == Token.TIPO_IDENTIFICADOR || this.token.getTipo() == Token.TIPO_INTEIRO
-                || this.token.getTipo() == Token.TIPO_REAL) {
+    private void Iterations() {
 
+        if (!this.token.getLexema().equals("while")) {
+            throw new SintaticException("while esperado, foi encontrado => " + this.token.getLexema().toString())
+        }
+
+        this.token = this.lexico.nextToken();
+
+        if (!this.token.getLexema().equals("(")) {
+            throw new SintaticException("( esperado para finalizar a declaração, ao lado de =>  " + this.token.getLexema().toString());
+        
+        }
+
+        this.token = this.lexico.nextToken();
+
+        this.relationalExp();
+
+        if (!this.token.getLexema().equals(")")) {
+            throw new SintaticException(") esperado para finalizar a declaração, ao lado de =>  " + this.token.getLexema().toString());
+        
+        }
+
+        this.token = this.lexico.nextToken();
+        this.Comando();
+
+
+    }
+
+    private void relationalExp() {
+        this.aritmeticExp();
+
+        if (this.token.getTipo() != Token.TIPO_OPERADOR_RELACIONAL) {
+            throw new SintaticException(
+                    "operador relacional esperado, foi encontrado =>  " + this.token.getLexema().toString());
+        }
+
+        this.token = this.lexico.nextToken();
+
+        this.aritmeticExp();
+    }
+
+    private void aritmeticExp() {
+    }
+
+    private void DecVariable() {
+
+        if (this.token.getLexema().equals("int") || this.token.getLexema().equals("float")
+                || this.token.getLexema().equals("char")) {
             this.token = this.lexico.nextToken();
 
         } else {
-            throw new RuntimeException(
-                    "Oxe, era para ser um identificador " + "ou número pertinho de " + this.token.getLexema());
+            throw new SintaticException("Necessario informar o tipo da id (int, float, char) Encontramos => "
+                    + this.token.getLexema().toString());
         }
-    }
 
-    private void OP() {
-        if (this.token.getTipo() == Token.TIPO_OPERADOR_ARITMETICO) {
+        if (this.token.getTipo() == Token.TIPO_IDENTIFICADOR) {
             this.token = this.lexico.nextToken();
-
         } else {
-            throw new RuntimeException(
-                    "Oxe, era para ser um operador aritmético (+, -, /, *) pertinho de " + this.token.getLexema());
+            throw new SintaticException(
+                    "Identificador é esperado aqui, Encontramos => " + this.token.getLexema().toString());
         }
+
+        if (this.token.getLexema().equals(";")) {
+            this.token = this.lexico.nextToken();
+        } else {
+            throw new SintaticException(
+                    "; esperado para finalizar a declaração, ao lado de => " + this.token.getLexema().toString());
+        }
+
+        if (this.token.getLexema().equals("int") || this.token.getLexema().equals("float")
+                || this.token.getLexema().equals("char")) {
+            DecVariable();
+
+        }
+
+        return;
+
     }
 }
