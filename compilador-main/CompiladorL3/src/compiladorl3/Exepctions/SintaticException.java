84b85b36fe32347433cf136aaaacbce9b46b9dc5
Victor Macaubas <victor@macaubas.com>
2022-11-29 15:45:35 -0300

    Criação da Exepctions

---
+++ compilador-main/CompiladorL3/src/compiladorl3/Exepctions/SintaticException.java
@@ -0,0 +1,10 @@
+package compiladorl3.Exepctions;
+
+public class SintaticException extends RuntimeException {
+    
+    public SintaticException (String messasge) {
+        super(messasge);
+    }
+
+
+}
